a58fd76c842d9a3c1dbe496470fa456f3d647279
Jonah Duckles <jonah@duckles.org>
2015-03-13 15:33:58 -0500

    initial attempt

--- readcosmos.py
+++ readcosmos.py
@@ -2,24 +2,44 @@ import telnetlib
 import time
 import json
 
-HOST="192.168.88.234"
-PORT=2000
+wait=0.3
 
-tn = telnetlib.Telnet(HOST, port=PORT)
+def connect(HOST,PORT):
+    return telnetlib.Telnet(HOST,port=PORT)
 
-tn.write('showversion\r\n')
-tn.read_eager()
+def runcommand(command, expect):
+    """ Wrapper to execute commands on COSMOS data loggers """
+    tn = telnetlib.Telnet(HOST,port=PORT)
+    tn.write(command)
+    #time.sleep(wait)
+    out = tn.read_until(expect,timeout=5)
+    tn.close()
+    return out
+     
+def checknpm(HOST,PORT):
+    return(runcommand('GetNPMTH'))
 
-tn.write('showheader')
-header = tn.read_until('\r\n')
-print header
+def getheader(command='showdataheader\r\n',expect="N1RH,"):
+    return runcommand(command, expect).strip()
+
+def getversion(command='showversion\r\n',expect='No Telem'):
+    return runcommand( command, expect).strip()
+
+def getdata():
+    command = 'rdata\r\n'
+    return runcommand(command, expect='\r\n').strip().split(',')
 
 
-while True:
-    tn.write('rdata\r\n')
-    record = tn.read_until('\r\n')
-    record = [ item.strip() for item in record.split(',') ]
-    print json.dumps(record)
-    time.sleep(10)
-
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print "%s <hostname> <port>" % (sys.argv[0])
+    HOST=sys.argv[1]
+    PORT=sys.argv[2]
+    #print getheader()
+    time.sleep(0.5)
+    header = "RecordNum,Time (UTC),P_mb, T1_degC,RH1,Vbat,N1C,N1ET(s),N1T(C),N1RH".split(',')
+    data = [item.lstrip().rstrip() for item in getdata()]
+    print json.dumps(dict(zip(header,data)),indent=2)
+   
 
